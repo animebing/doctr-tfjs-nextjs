eba10a6f939d438706e22f2660c7cf0609a41c4b
Yanbing Dong <dongyanbing@metasota.ai>
2024-01-15 18:20:25 +0800

    update

---
+++ app/components/VisionWrapper/index.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default () => {
+  return (
+    <div className="flex flex-col mb-2">
+      <div className="flex items-center gap-2 mb-5">
+        <img src="/doctr.svg" alt="logo" />
+        <h2 className="text-2xl">Welcome to the docTR by mindee live demo page</h2>
+      </div>
+    </div>
+  );
+};
