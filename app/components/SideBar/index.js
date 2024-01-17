77ef3c3a189e128dd46a16af52fd8f4888620a12
Yanbing Dong <dongyanbing@metasota.ai>
2024-01-17 18:28:20 +0800

    update

--- app/components/SideBar/index.js
+++ app/components/SideBar/index.js
@@ -1,7 +1,39 @@
 'use client';
+import Select from 'react-select';
+import { DET_CONFIG, RECO_CONFIG } from "@app/common/constants";
 
-export default () => {
+export default ({
+  detConfig,
+  setDetConfig,
+  recoConfig,
+  setRecoConfig,
+}) => {
   return (
-    <div>SideBar</div>
+    <div className="p-8 rounded-lg border-2 flex flex-col gap-2">
+      <div className="text-[18px] font-medium mb-5">
+        1 - Select a Model
+      </div>
+      <div className="flex flex-col gap-[5px]">
+        <p className="text-[14px]">Detection model</p>
+        <Select
+          defaultValue={detConfig}
+          onChange={setDetConfig}
+          options={Object.values(DET_CONFIG)}
+        />
+      </div>
+      <div className="flex flex-col gap-[5px]">
+        <p className="text-[14px]">Recognition model</p>
+        <Select
+          defaultValue={recoConfig}
+          onChange={setRecoConfig}
+          options={Object.values(RECO_CONFIG)}
+        />
+      </div>
+      <div className="text-[18px] font-medium mb-5 mt-[10px]">
+      2 - Upload an image
+      </div>
+      
+
+    </div>
   );
 };
--- app/components/VisionWrapper/index.js
+++ app/components/VisionWrapper/index.js
@@ -3,12 +3,21 @@
 import SideBar from '@/app/components/SideBar'
 import AnnotationViewer from '@/app/components/AnnotationViewer'
 import WordsList from '@/app/components/WordsList';
+import { DET_CONFIG, RECO_CONFIG } from "@/app/common/constants";
 
 export default () => {
+  const [detConfig, setDetConfig] = useState(DET_CONFIG.db_mobilenet_v2);
+  const [recoConfig, setRecoConfig] = useState(RECO_CONFIG.crnn_vgg16_bn);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-12">
       <div className="col-span-1 md:col-span-3">
-        <SideBar />
+      <Sidebar
+        detConfig={detConfig}
+        setDetConfig={setDetConfig}
+        recoConfig={recoConfig}
+        setRecoConfig={setRecoConfig}
+      />
       </div>
       <div className="col-span-1 md:col-span-5">
         <AnnotationViewer />
