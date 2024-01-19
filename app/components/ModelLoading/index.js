c3762b204f2cb1425e40e0aeaeaa0e328aa7cb5b
Yanbing Dong <dongyanbing@metasota.ai>
2024-01-19 18:13:23 +0800

    update

--- app/components/ModelLoading/index.js
+++ app/components/ModelLoading/index.js
@@ -2,8 +2,13 @@ import { ThreeDots } from 'react-loader-spinner'
 
 export default () => {
   return (
-      <div className="absolute top-0 left-0 w-full h-full bg-white/80 flex justify-center items-center">
-        <ThreeDots visible={true} color="#4fa94d" height={80} width={80} />
+      <div className="fixed z-10 top-0 left-0 w-full h-full bg-white/80 flex justify-center items-center">
+        <div className='flex flex-col justify-center items-center'>
+          <ThreeDots visible={true} color="#4fa94d" height={40} width={80} />
+          <div className='text-center text-[#4fa94d] text-[20px]'>
+            Loading Model
+          </div>
+        </div>
       </div>
   );
 };
--- app/components/SideBar/index.js
+++ app/components/SideBar/index.js
@@ -10,31 +10,33 @@ export default ({
 }) => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-[18px] font-medium mb-2">
+      <div className="text-sm lg:text-lg font-medium mb-2">
         1 - Select a Model
       </div>
       <div className="flex flex-col gap-[5px] mb-1">
-        <p className="text-[14px]">Detection model</p>
+        <p className="lg:text-base">Detection model</p>
         <Select
+          className='lg:text-sm'
           defaultValue={detConfig}
           onChange={setDetConfig}
           options={Object.values(DET_CONFIG)}
         />
       </div>
       <div className="flex flex-col gap-[5px] mb-1">
-        <p className="text-[14px]">Recognition model</p>
+        <p className="text-[10px] lg:text-base">Recognition model</p>
         <Select
+          className='lg:text-sm'
           defaultValue={recoConfig}
           onChange={setRecoConfig}
           options={Object.values(RECO_CONFIG)}
         />
       </div>
-      <div className="text-[18px] font-medium mb-3 mt-[10px]">
+      <div className="text-sm lg:text-lg mb-3 mt-[10px]">
         2 - Upload an image
       </div>
       <div className='h-56 rounded-[4px] border-[1px] border-[#E6E9EC] flex flex-col gap-[10px] justify-center items-center'>
         <img src="/image-placeholder.svg" alt="placeholder" className='h-[100px] rounded-lg object-contain cursor-pointer'/>
-        <div className='text-[15px] text-center'>
+        <div className='text-[14px] text-center'>
         Upload an image <br />
         (.jpg, .png, .webp)
         </div>
