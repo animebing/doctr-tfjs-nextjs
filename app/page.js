b64a279dc105cab3575dd3d76f7f59ebc57134ef
animebing <yanbing_dong@hotmail.com>
2024-01-19 09:15:19 +0800

    add model loading spinner

--- app/page.js
+++ app/page.js
@@ -1,16 +1,28 @@
 "use client";
 
+import { useState } from 'react';
+
+import ModelLoading from '@/app/components/ModelLoading'
 import WelcomeMessage from '@/app/components/WelcomeMessage'
 import VisionWrapper from '@/app/components/VisionWrapper'
 import PageFooter from '@/app/components/PageFooter'
 
 
 export default () => {
+  const [detLoading, setDetLoading] = useState(false);
+  const [recoLoading, setRecoLoading] = useState(false);
+
   return (
-    <div className='flex flex-col gap-2 px-[42px] pt-[21px]'>
-      <WelcomeMessage />
-      <VisionWrapper />
-      <PageFooter />
-    </div>
+    <>
+      {(detLoading || recoLoading) && <ModelLoading />}
+      <div className='flex flex-col gap-2 px-[42px] pt-[21px]'>
+        <WelcomeMessage />
+        <VisionWrapper
+          setDetLoading={setDetLoading} 
+          setRecoLoading={setRecoLoading} 
+        />
+        <PageFooter />
+      </div>
+    </>
   );
 };
