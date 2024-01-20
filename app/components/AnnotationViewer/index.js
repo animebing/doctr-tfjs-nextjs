92b1d243a060466365b671b33a6f40673433bbbf
animebing <yanbing_dong@hotmail.com>
2024-01-21 00:16:57 +0800

    view image

--- app/components/AnnotationViewer/index.js
+++ app/components/AnnotationViewer/index.js
@@ -1,16 +1,49 @@
-'use client';
+'use client'
 
-export default () => {
+import dynamic from 'next/dynamic'
+const AnnotationViewerBase = dynamic(() =>
+  import('react-mindee-js').then((mod) => mod.AnnotationViewer),
+  // { ssr: false }, // still okay without this
+);
+import { TailSpin } from 'react-loader-spinner';
+
+
+export default ({
+  loadingImage,
+  annotationData,
+  setAnnotationStage,
+}) => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-sm lg:text-lg font-medium mb-3">
+      <div className="text-base lg:text-lg font-medium mb-3">
         3 - Visualize word predictions
       </div>
-      <div className="h-[430px] rounded-[4px] border-[1px] border-[#E6E9EC] flex justify-center items-center">
-        <div className="text-sm">
-          No image uploaded yet
+      {loadingImage ? (
+        <div className="h-[435px] rounded border-x border-y border-gray-200 flex justify-center items-center">
+          <TailSpin
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+          />
         </div>
-      </div>
+      ) : !annotationData.image ? (
+        <div className="h-[435px] rounded border-x border-y border-gray-200 flex justify-center items-center">
+          <div className="text-xs lg:text-sm">
+            No image uploaded yet
+          </div>
+        </div>
+      ) : (
+        <div className='flex justify-center'>
+          <AnnotationViewerBase
+            data={annotationData}
+            getStage={setAnnotationStage}
+            style={{ height: "435px", width: "95%", background: "white" }}
+          />
+        </div>
+      )}
     </div>
   );
 };
