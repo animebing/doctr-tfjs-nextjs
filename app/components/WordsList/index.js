4d2ac0f7940559e0c05c7088e8d1be11044dccbd
animebing <yanbing_dong@hotmail.com>
2024-01-21 00:13:05 +0800

    update

--- app/components/WordsList/index.js
+++ app/components/WordsList/index.js
@@ -3,11 +3,11 @@
 export default () => {
   return (
     <div className="flex flex-col gap-2">
-      <div className="text-sm lg:text-lg font-medium mb-3">
+      <div className="text-base lg:text-lg font-medium mb-3">
         4 - Visualize word values
       </div>
-      <div className="h-[430px] rounded-[4px] border-[1px] border-[#E6E9EC] flex justify-center items-center">
-        <div className="text-sm">
+      <div className="h-[435px] rounded border-x border-y border-gray-200 flex justify-center items-center">
+        <div className="text-xs lg:text-sm">
           No image uploaded yet
         </div>
       </div>
