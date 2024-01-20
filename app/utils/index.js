f70688b57b31262f7e1df819eea1829e4ac96e94
animebing <yanbing_dong@hotmail.com>
2024-01-21 00:11:08 +0800

    add getHeatMapFromImage

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,38 +1,79 @@
 'use client';
 
-import {loadGraphModel} from '@tensorflow/tfjs'
+import {
+  browser,
+  loadGraphModel,
+  scalar,
+  squeeze,
+} from '@tensorflow/tfjs'
 
-function sleep(timeout) {
+import { DET_MEAN, DET_STD } from '@/app/common/constants';
+
+export function sleep(timeout) {
   return new Promise(resolve => setTimeout(resolve, timeout));
 }
 
 
 export const loadDetectionModel = async ({
-  setDetLoading,
+  setLoadingDetModel,
   detectionModel,
   detConfig,
 }) => {
-  setDetLoading(true);
+  setLoadingDetModel(true);
   try {
     detectionModel.current = await loadGraphModel(detConfig.path);
   } catch (error) {
     console.log(error);
   } finally {
-    setDetLoading(false);
+    setLoadingDetModel(false);
   }
 };
 
 export const loadRecognitionModel = async ({
-  setRecoLoading,
+  setLoadingRecoModel,
   recognitionModel,
   recoConfig,
 }) => {
-  setRecoLoading(true);
+  setLoadingRecoModel(true);
   try {
     recognitionModel.current = await loadGraphModel(recoConfig.path);
   } catch (error) {
     console.log(error);
   } finally {
-    setRecoLoading(false);
+    setLoadingRecoModel(false);
   }
 };
+
+
+export const getImageTensorForDetectionModel = (
+  imageObject,
+  size,
+) => {
+  let tensor = browser
+    .fromPixels(imageObject)
+    .resizeNearestNeighbor(size)
+    .toFloat();
+  let mean = scalar(255 * DET_MEAN);
+  let std = scalar(255 * DET_STD);
+  return tensor.sub(mean).div(std).expandDims();
+};
+
+
+export const getHeatMapFromImage = async ({
+  heatmapContainer,
+  detectionModel,
+  imageObject,
+  size,
+}) =>
+  new Promise(async (resolve) => {
+    heatmapContainer.width = imageObject.width;
+    heatmapContainer.height = imageObject.height;
+    let tensor = getImageTensorForDetectionModel(imageObject, size);
+    let prediction = await detectionModel?.execute(tensor);
+    prediction = squeeze(prediction, 0);
+    if (Array.isArray(prediction)) {
+      prediction = prediction[0];
+    }
+    await browser.toPixels(prediction, heatmapContainer);
+    resolve("detection");
+  });
