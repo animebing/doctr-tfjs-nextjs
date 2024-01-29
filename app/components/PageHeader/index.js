751d800c6b5932f817173e0108f8cfc507d656bb
animebing <yanbing_dong@hotmail.com>
2024-01-30 01:07:58 +0800

    page header

---
+++ app/components/PageHeader/index.js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div className="flex px-2 py-7 lg:px-14 mb-4 lg:mb-0 border-b-2 border-solid border-gray-100">
+      <img src="/logo.svg" alt="logo" className="h-7" />
+    </div>
+  );
+};
